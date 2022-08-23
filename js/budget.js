d0ec43a95fbc66a0e34028d3d1dc2506a4dd0745
farhanchy99 <farhanchowdhury1999@gmail.com>
2022-08-23 20:25:40 +0600

    7th  commit

--- js/budget.js
+++ js/budget.js
@@ -1,30 +1,62 @@
+//Button Disable Section
+
+document.getElementById('btn-1').addEventListener('click', function () {
+    document.getElementById('btn-1').disabled = true;
+    document.getElementById('btn-1').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-2').addEventListener('click', function () {
+    document.getElementById('btn-2').disabled = true;
+    document.getElementById('btn-2').style.backgroundColor = '#717170';
+})
+document.getElementById('btn-3').addEventListener('click', function () {
+    document.getElementById('btn-3').disabled = true;
+    document.getElementById('btn-3').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-4').addEventListener('click', function () {
+    document.getElementById('btn-4').disabled = true;
+    document.getElementById('btn-4').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-5').addEventListener('click', function () {
+    document.getElementById('btn-5').disabled = true;
+    document.getElementById('btn-5').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-6').addEventListener('click', function () {
+    document.getElementById('btn-6').disabled = true;
+    document.getElementById('btn-6').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-7').addEventListener('click', function () {
+    document.getElementById('btn-7').disabled = true;
+    document.getElementById('btn-7').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-8').addEventListener('click', function () {
+    document.getElementById('btn-8').disabled = true;
+    document.getElementById('btn-8').style.backgroundColor = '#717170';
+})
+
+document.getElementById('btn-9').addEventListener('click', function () {
+    document.getElementById('btn-9').disabled = true;
+    document.getElementById('btn-9').style.backgroundColor = '#717170';
+})
 
 
+//Budget Section
+document.getElementById('btn-buy').addEventListener('click', function(){
+    const inputPlayers = document.getElementsByTagName("tr").length;
+    const inputAmount = parseInt(document.getElementById('players-input-field').value);
+    document.getElementById('playerexpense').innerText = inputPlayers * inputAmount;
+})
 
-
-
-
-
-
-// document.getElementById('btn-deposit').addEventListener('click', function () {
-//     /* 
-//     1. get the element by id
-//     2. get the value from the element
-//     3. convert string value to a number
-//     */
-//     const newDepositAmount = getInputFieldValueById('deposit-field');
-//     /* 
-//     1. get previous deposit total by id
-//     */
-//     const previousDepositTotal = getTextElementValueById('deposit-total');
-    
-//     // calculate new deposit total
-//     const newDepositTotal = previousDepositTotal + newDepositAmount;
-//     // set deposit total value
-//     setTextElementValueById('deposit-total', newDepositTotal);
-
-//     // get previous balance by using the function
-//     const previousBalanceTotal = getTextElementValueById('deposit-total')
-//     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-//     setTextElementValueById('deposit-total', newBalanceTotal);
-// })
+//TotalBudget Section
+document.getElementById('btn-total').addEventListener('click', function(){
+    const coach = parseInt(document.getElementById('coach').value);
+    const manager = parseInt(document.getElementById('manager').value);
+    const playerExpense = parseInt(document.getElementById('playerexpense').innerText);
+    const total = coach + manager + playerExpense
+    document.getElementById('total').innerText = total; 
+})
